90b01294d3d98545eb11d19a3f044154acdee214
Einar Holsbø <einar@cs.uit.no>
2015-03-05 15:39:06 +0100

    Implemented a weak learner

--- grow.R
+++ grow.R
@@ -28,7 +28,42 @@ impurity = function (labels, measure = "gini") {
   sum(measures)
 }
 
+# small test to check whether the impurity measure acts as expected
+# test = rep(0, 100)
+# res = rep(-1, 100)
+# for (i in 1:100) {
+#   test[i] = 1
+#   res[i] = impurity(test, measure="entropy")
+# }
+# plot(res)
+
 # axis-aligned split f'n. Assumes the feature subspace has already been chosen
 split.axis = function (data, labels) {
+  classes = unique(labels)
+  lowestImpurity = .Machine$double.xmax  # biggest float there is
+  splitval = NA
+  splitvar = 0
 
+  for (predictor in 1:ncol(data)) {
+    # pick  a handful of potential split points
+    # NB that you can inject randomness into the model by adjusting the n.o
+    # random splits you evaluate
+    splits = runif(50, min(data[,predictor]), max(data[,predictor]))
+
+    for (t in splits) {
+      leftIdx = data[,predictor] < t
+
+      impurity = evaluateSplit(labels[leftIdx], labels[!leftIdx])
+      if (impurity < lowestImpurity) {
+        splitval = t
+        splitvar = predictor
+        lowestImpurity = impurity
+      }
+    }
+  }
+  list(predictor=splitvar, threshold=splitval, impurity=lowestImpurity)
+}
+
+evaluateSplit = function (A, B) {
+  impurity(A)/length(A) + impurity(B)/length(B)
 }
